8b7c1aa9c4e1c60e00bdee7a644eaef2120030c9
ayun1998-3 <austenyun2004@gmail.com>
2024-06-13 10:58:41 -0400

    first commit

---
+++ rf_rw_platform/psu_cortexa53_0/standalone_psu_cortexa53_0/bsp/libsrc/build_configs/gen_bsp/libsrc/libmetal/src/lib/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/IECS-Lab/Desktop/vitis_2024_workspace/rf_rw_platform/psu_cortexa53_0/standalone_psu_cortexa53_0/bsp/libsrc/libmetal/src/lib
+# Build directory: C:/Users/IECS-Lab/Desktop/vitis_2024_workspace/rf_rw_platform/psu_cortexa53_0/standalone_psu_cortexa53_0/bsp/libsrc/build_configs/gen_bsp/libsrc/libmetal/src/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("compiler")
+subdirs("processor")
+subdirs("system")
---
+++ rf_rw_platform/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/libsrc/libmetal/src/lib/system/generic/xlnx/microblaze_generic/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/IECS-Lab/Desktop/vitis_2024_workspace/rf_rw_platform/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/libmetal/src/lib/system/generic/xlnx/microblaze_generic
+# Build directory: C:/Users/IECS-Lab/Desktop/vitis_2024_workspace/rf_rw_platform/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/libsrc/libmetal/src/lib/system/generic/xlnx/microblaze_generic
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
